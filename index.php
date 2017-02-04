5cf0ad528774f055f555067494ac1a4620c1fc27
Thibaut <thibaut.emion@gmail.com>
2017-02-04 23:31:20 +0100

    Test

--- index.php
+++ index.php
@@ -9,5 +9,12 @@ pied();
 function contenu()
 { ?>
     <h1>Bonjour à tous</h1>
-    <h2> Toto  </h2>
+    <h2> Toto </h2>
+
+    <button class="ui primary button">
+        Save
+    </button>
+    <button class="ui button">
+        Discard
+    </button>
 <?php } ?>
